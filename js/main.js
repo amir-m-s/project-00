6c8c95caf0fcb9e47084ae3dc2d4d12970f8e027
amir-m-s <71013189+amir-m-s@users.noreply.github.com>
2023-07-09 22:58:42 +0330

    Update project
    Add functionality to menu button

--- js/main.js
+++ js/main.js
@@ -1,15 +1,54 @@
 const mobile_menu = document.querySelector("#mobile-menu");
 const tools = document.querySelector("#tools");
 
+const submenu_1 = document.querySelector("#sub-menu-1");
+const submenu_2 = document.querySelector("#sub-menu-2");
+const submenu_3 = document.querySelector("#sub-menu-3");
+const submenu_4 = document.querySelector("#sub-menu-4");
+
 function toggle_menu() {
     let menu_display = getComputedStyle(mobile_menu).display;
     if (menu_display == "none") {
-        mobile_menu.style.transition = "0.3s ease-in-out";
-        tools.style.transition = "0.3s ease-in-out";
+        mobile_menu.style.animation = "ease-in-out";
+        tools.style.animation = "ease-in-out";
+        tools.style.ani
         mobile_menu.style.display = "flex";
         tools.style.display = "flex";
     } else {
         mobile_menu.style.display = "none";
         tools.style.display = "none";
+    }
+}
+
+function submenu_manager(id) {
+    switch (id) {
+        case "submenu-1": {
+            submenu_1.style.display = "flex";
+            submenu_2.style.display = "none";
+            submenu_3.style.display = "none";
+            submenu_4.style.display = "none";
+            break;
+        }
+        case "submenu-2": {
+            submenu_1.style.display = "none";
+            submenu_2.style.display = "flex";
+            submenu_3.style.display = "none";
+            submenu_4.style.display = "none";
+            break;
+        }
+        case "submenu-3": {
+            submenu_1.style.display = "none";
+            submenu_2.style.display = "none";
+            submenu_3.style.display = "flex";
+            submenu_4.style.display = "none";
+            break;
+        }
+        case "submenu-4": {
+            submenu_1.style.display = "none";
+            submenu_2.style.display = "none";
+            submenu_3.style.display = "none";
+            submenu_4.style.display = "flex";
+            break;
+        }
     }
 }
