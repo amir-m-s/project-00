de88316ea042dca21f7c5a9cd38cac9f435155f5
amir-m-s <71013189+amir-m-s@users.noreply.github.com>
2023-08-12 14:09:19 +0330

    Update main.js
    Add comments and make code prettier by reordering and sorting stuff

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
+// Mobile version variables
 const mobile_menu = document.querySelector("#mobile-menu");
 const tools = document.querySelector("#tools");
 
-const submenu_1 = document.querySelector("#sub-menu-1");
-const submenu_2 = document.querySelector("#sub-menu-2");
-const submenu_3 = document.querySelector("#sub-menu-3");
-const submenu_4 = document.querySelector("#sub-menu-4");
 
 function toggle_menu() {
     let menu_display = getComputedStyle(mobile_menu).display;
@@ -17,6 +14,14 @@ function toggle_menu() {
     }
 }
 
+
+// Footer submenu variables
+const submenu_1 = document.querySelector("#sub-menu-1");
+const submenu_2 = document.querySelector("#sub-menu-2");
+const submenu_3 = document.querySelector("#sub-menu-3");
+const submenu_4 = document.querySelector("#sub-menu-4");
+
+// Submenu functionality
 function submenu_manager(id) {
     switch (id) {
         case "submenu-1": {
@@ -50,25 +55,32 @@ function submenu_manager(id) {
     }
 }
 
+// Go to top button variable
 const go_top_btn = document.querySelector(".gotop");
 
+// Go to top functionality
 go_top_btn.addEventListener("click", () => {
     window.scrollTo({top: 0, behavior: "smooth"});
 })
 
+// Search modal variables
 const search_btn = document.querySelector(".search > div");
 const search_modal = document.querySelector("#search-modal");
 const search_modal_close = document.querySelector("#search-modal-close-btn");
 
+// Search modal functionalities
+// Search button event listener
 search_btn.addEventListener("click", () => {
     search_modal.style.display = "block";
 });
 
+// If user clicks anywhere besides searchbar and search button, modal will hide
 search_modal.addEventListener("click", (e) => {
     if (e.target.id == "search-modal")
         search_modal.style.display = "none";
 });
 
+// Search modal close button functionality
 search_modal_close.addEventListener("click", () => {
     search_modal.style.display = "none";
 });
